9e9501b3530143e369b4234685835325e8d2c141
Carlos Yair Larracilla <carlosyairlarracilla@MacBook-Pro-de-Carlos.local>
2022-08-26 22:35:02 -0500

    feat(main): first commit [HOUSE-1]

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Desestructuracion } from "./components/Desestructuracion";
+
+
+function App() {
+
+  return (
+    <>
+      <Desestructuracion />
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/Desestructuracion.tsx
@@ -0,0 +1,30 @@
+interface Carro {
+    color?: 'red' | 'yellow';
+    modelo: number;
+    placa: string;
+    plazas: number;
+    avanzar: () => void;
+    frenar: (marca: string) => void;
+}
+
+export const Desestructuracion = () => {
+
+    const carro: Carro = {
+        color: 'red',
+        modelo: 2020,
+        placa: 'YBP304',
+        plazas: 4,
+        avanzar: () => console.log('Avanza'),
+        frenar: (marca) => console.log('Frena, ', marca)
+    };
+
+    const { avanzar } = carro;
+
+    console.log(`Color: ${carro.color && carro.color}`);
+    avanzar();
+    carro.frenar('Ferrari');
+
+  return (
+    <div>Desestructuracion</div>
+  )
+}
